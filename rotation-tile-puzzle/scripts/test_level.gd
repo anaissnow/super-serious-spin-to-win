feec4e82c44cf168a745e77b0ca168dda0fa469b
Anais Snow <80048741+anaissnow@users.noreply.github.com>
2026-06-21 23:36:10 +0800

    map tile setup

---
+++ rotation-tile-puzzle/scripts/test_level.gd
@@ -0,0 +1,13 @@
+extends Node
+
+
+func _on_rotate_clockwise_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_rotate_counterclockwise_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_apply_pressed() -> void:
+	pass # Replace with function body.
