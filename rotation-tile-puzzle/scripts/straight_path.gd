1693dad6d13ba5f36185e0ed4449408719840ddc
Anais Snow <80048741+anaissnow@users.noreply.github.com>
2026-06-23 01:31:56 +0800

    tweaking the tiles again

--- rotation-tile-puzzle/scripts/straight_path.gd
+++ rotation-tile-puzzle/scripts/straight_path.gd
@@ -1,15 +1,15 @@
-extends TextureRect
+extends Sprite2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func rotate_zero():
-	animation_player.play("rotation 0")
+	animation_player.play("rotate 0")
 
 func rotate_ninety():
 	animation_player.play("rotation 90")
 
 func rotate_anticlockwise_zero():
-	animation_player.play_backwards("rotation 0")
+	animation_player.play_backwards("rotate 0")
 
 func rotate_anticlockwise_ninety():
 	animation_player.play_backwards("rotation 90")
--- rotation-tile-puzzle/scripts/test_level.gd
+++ rotation-tile-puzzle/scripts/test_level.gd
@@ -1,18 +1,13 @@
 extends Node
 
-@onready var grass: TextureRect = $MarginContainer/HBoxContainer/GridContainer/Grass
-@onready var dead_end: TextureRect = $MarginContainer/HBoxContainer/GridContainer/DeadEnd
-@onready var curved_path: TextureRect = $MarginContainer/HBoxContainer/GridContainer/CurvedPath
-@onready var straight_path: TextureRect = $MarginContainer/HBoxContainer/GridContainer/StraightPath
+@onready var straight_path: Sprite2D = $MapTiles/StraightPath
+@onready var curved_path: Sprite2D = $MapTiles/CurvedPath
+@onready var dead_end: Sprite2D = $MapTiles/DeadEnd
+@onready var grass: Sprite2D = $MapTiles/Grass
 
 
 func _on_rotate_clockwise_pressed() -> void:
-	if straight_path.position.x == 0:
-		if straight_path.has_method("rotate_ninety"):
-			straight_path.rotate_ninety()
-	else:
-		if straight_path.has_method("rotate_zero"):
-			straight_path.rotate_zero()
+	pass
 
 
 func _on_rotate_counterclockwise_pressed() -> void:
