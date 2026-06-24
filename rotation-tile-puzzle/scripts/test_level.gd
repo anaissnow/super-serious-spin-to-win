e046d323d5cb45b4a8a672c31b5c6fa823789239
Anais Snow <80048741+anaissnow@users.noreply.github.com>
2026-06-24 18:58:05 +0800

    it can all rotate now
    added new map tiles, added rotate code, need to tweak more on getting random tiles on the map and make it... offset (more centered)

--- rotation-tile-puzzle/scripts/test_level.gd
+++ rotation-tile-puzzle/scripts/test_level.gd
@@ -1,17 +1,70 @@
-extends Node
+extends Node2D
 
-@onready var straight_path: Sprite2D = $MapTiles/StraightPath
-@onready var curved_path: Sprite2D = $MapTiles/CurvedPath
-@onready var dead_end: Sprite2D = $MapTiles/DeadEnd
-@onready var grass: Sprite2D = $MapTiles/Grass
 
+@onready var tile_map_layer: TileMapLayer = $TileMapLayer
+
+
+@export var GridSize = 5
+var tile_dict = {}
+
+
+# helper variable
+var flip_h := TileSetAtlasSource.TRANSFORM_FLIP_H
+var flip_v := TileSetAtlasSource.TRANSFORM_FLIP_V
+var transpose := TileSetAtlasSource.TRANSFORM_TRANSPOSE
+
+
+var tile_transforms := {
+	Vector2i(0, -1): [0],
+	Vector2i(1, 0): [flip_h, transpose],
+	Vector2i(0, 1): [flip_v, flip_h],
+	Vector2i(-1, 0): [flip_v, transpose]
+}
+
+var tile_direction := Vector2i(0, -1)
+var applied_transform : int
+
+
+func _ready() -> void:
+	for x in GridSize:
+		for y in GridSize:
+			tile_dict[str(Vector2i(x, y))] ={
+				"Type": "Grass",
+				#"Type": "Straight Path",
+				#"Type": "Curved Path",
+				#"Type": "Dead End"
+			}
+			tile_map_layer.set_cell(Vector2i(x, y), 0, Vector2i(0, 0), 0)
 
 func _on_rotate_clockwise_pressed() -> void:
-	pass
+	rotate_tile("cw")
 
 
 func _on_rotate_counterclockwise_pressed() -> void:
-	pass # Replace with function body.
+	rotate_tile("ccw")
+
+
+func rotate_tile(dir):
+	# update the direction vector
+	if dir == "cw":
+		tile_direction = Vector2i(tile_direction[1] * -1, tile_direction[0])
+	else:
+		tile_direction = Vector2i(tile_direction[1], tile_direction[0] * -1)
+	get_applied_transform()
+	redraw_tiles()
+
+
+func get_applied_transform():
+	# then reset the applied transform and work out the new one based on rotation and mirror
+	applied_transform = 0
+	for i in tile_transforms[tile_direction]:
+		applied_transform += i
+
+
+func redraw_tiles():
+	for x in GridSize:
+		for y in GridSize:
+			tile_map_layer.set_cell(Vector2i(x, y), 0, Vector2i(0, 0), applied_transform)
 
 
 func _on_apply_pressed() -> void:
