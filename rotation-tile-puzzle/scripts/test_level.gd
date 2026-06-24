5f7bef8396dfed618cc9d3d8c55ccb8f3d01a7fc
Anais Snow <80048741+anaissnow@users.noreply.github.com>
2026-06-25 01:13:40 +0800

    rotate function fixed
    code can access the randomly generated map in _ready() and then can rotate the generated tiles without problems, and reset button is added

--- rotation-tile-puzzle/scripts/test_level.gd
+++ rotation-tile-puzzle/scripts/test_level.gd
@@ -5,7 +5,16 @@ extends Node2D
 
 
 @export var GridSize = 5
-var tile_dict = {}
+var TileDict : Dictionary = {
+	"Grass" : Vector2i(0, 0),
+	"Straight Path" : Vector2i(1, 0),
+	"Curved Path" : Vector2i(2, 0),
+	"Dead End" : Vector2i(3, 0)
+}
+var random_type = TileDict.keys().pick_random()
+var atlas_coords = TileDict[random_type]
+var generated_tiles = []
+
 
 
 # helper variable
@@ -26,15 +35,27 @@ var applied_transform : int
 
 
 func _ready() -> void:
+	generate_map()
+	#for x in GridSize:
+		#for y in GridSize:
+			#var tile_name = TileDict.keys().pick_random()
+			#
+			#tile_map_layer.set_cell(Vector2i(x, y), 0, TileDict[tile_name], 0)
+
+
+func generate_map():
+	generated_tiles.clear()
+	
 	for x in GridSize:
 		for y in GridSize:
-			tile_dict[str(Vector2i(x, y))] ={
-				"Type": "Grass",
-				#"Type": "Straight Path",
-				#"Type": "Curved Path",
-				#"Type": "Dead End"
-			}
-			tile_map_layer.set_cell(Vector2i(x, y), 0, Vector2i(0, 0), 0)
+			var tile_name = TileDict.keys().pick_random()
+			var atlas = TileDict[tile_name]
+			print(atlas)
+			generated_tiles.append({
+				"Cell" : Vector2i(x, y),
+				"Atlas" : atlas,
+			})
+			tile_map_layer.set_cell(Vector2i(x, y), 0, atlas)
 
 func _on_rotate_clockwise_pressed() -> void:
 	rotate_tile("cw")
@@ -62,10 +83,17 @@ func get_applied_transform():
 
 
 func redraw_tiles():
-	for x in GridSize:
-		for y in GridSize:
-			tile_map_layer.set_cell(Vector2i(x, y), 0, Vector2i(0, 0), applied_transform)
+	for tile in generated_tiles:
+		var cell = tile["Cell"]
+		var atlas = tile["Atlas"]
+		for x in GridSize:
+			for y in GridSize:
+				tile_map_layer.set_cell(cell, 0, atlas, applied_transform)
 
 
 func _on_apply_pressed() -> void:
 	pass # Replace with function body.
+
+
+func _on_reset_pressed() -> void:
+	get_tree().reload_current_scene()
